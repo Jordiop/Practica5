92e66e121f0909fa57f58e404b3700b7f389c439
Jordiop <jordiosarenkhoe@outlook.es>
2022-05-15 15:05:46 +0200

    Versión final

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,30 +1,34 @@
 public class Calculadora implements ICalculadora{
-    // A y B hacen referencia al Primer número y al segundo
+    // A y B hacen referencia al primer número y al segundo
     private float a;
     private float b;
 
-
+	// Constructor de calculadora
     public Calculadora(float a, float b) {
         this.a = a;
         this.b = b;
 
     }
 
+	// Método suma
     @Override
     public float suma(float a, float b){
         return a + b;
     }
 
+	// Método resta
     @Override
     public float resta(float a, float b){
         return a - b;
     }
 
+	// Método multiplicación
     @Override
     public float multiplicacion(float a, float b){
         return a * b;
     }
 
+	// Método división
     @Override
     public float division (float a, float b) {
         return (a/b);
--- src/main/java/CalculadoraTest.java
+++ src/main/java/CalculadoraTest.java
@@ -3,12 +3,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculadoraTest {
+	
+		// Tests de los 4 métodos.
 
         @Test
         void testSuma() {
             Calculadora calculadora = new Calculadora(5,6);
             float c = calculadora.suma(5,6);
             assertEquals(11, c,0);
+			/* 
+			*	11 es el resultado esperado, 
+			*	c es nuestra variable con el método realizando dicho procedimiento 
+			*	0 es el delta, que es el máximo error que queremos permitir. 
+			*/
 
         }
 
@@ -17,6 +24,11 @@ public class CalculadoraTest {
             Calculadora calculadora = new Calculadora(8,6);
             float c = calculadora.resta(8,6);
             assertEquals(2, c,0);
+			/* 
+			*	2 es el resultado esperado, 
+			*	c es nuestra variable con el método realizando dicho procedimiento 
+			*	0 es el delta, que es el máximo error que queremos permitir. 
+			*/
         }
 
         @Test
@@ -24,6 +36,11 @@ public class CalculadoraTest {
             Calculadora calculadora = new Calculadora(5,4);
             float c = calculadora.multiplicacion(5,4);
             assertEquals(20, c,0);
+			/* 
+			*	20 es el resultado esperado, 
+			*	c es nuestra variable con el método realizando dicho procedimiento 
+			*	0 es el delta, que es el máximo error que queremos permitir. 
+			*/
         }
 
         @Test
@@ -31,6 +48,11 @@ public class CalculadoraTest {
             Calculadora calculadora = new Calculadora(7,2);
             float c = calculadora.division(7,2);
             assertEquals(3.5, c,0);
+			/* 
+			*	3.5 es el resultado esperado, 
+			*	c es nuestra variable con el método realizando dicho procedimiento 
+			*	0 es el delta, que es el máximo error que queremos permitir. 
+			*/
         }
 
     }
